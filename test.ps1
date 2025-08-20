16f9d495a2ad360c1cfa6578def27e9d7c408805
c23cs1002 <161812194+c23cs1002@users.noreply.github.com>
2025-08-20 20:24:06 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,2 @@
 $info = "Hello world"
-Invoke-RestMethod -Uri "https://webhook.site/75d4e516-a529-48b5-9fd2-6f3bd0bc8767" -Method Post -Body @{data=$info}
+Invoke-RestMethod -Uri " https://webhook.site/1c5b955b-3946-4485-a55c-41382b24ae2b" -Method Post -Body @{data=$info}
